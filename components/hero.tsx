bdbd50e1bfe2ba34463f10efc1e9ccfb3e081e51
Bhim Prasad Adhikari <134574549+BhimPrasadAdhikari@users.noreply.github.com>
2025-09-27 18:01:51 +0530

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,7 +15,7 @@ export function Hero() {
         className="absolute inset-0 w-full h-full object-cover"
         aria-hidden="true"
       >
-        <source src="/hero.mp4" type="video/mp4" />
+        <source src="/farmer_walks.mp4" type="video/mp4" />
       </video>
 
       {/* Gradient Overlay */}
